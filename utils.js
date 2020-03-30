edd9b06db2ba3bd7baec85b217ff292af6a80712
JackLovel <gogkatsu@outlook.com>
2020-03-30 20:51:46 +0800

    update

---
+++ utils.js
@@ -0,0 +1,61 @@
+var log = console.log.bind(console)
+
+var imageFromPath = function(path) {
+    var img = new Image()
+    img.src = path
+    return img
+}
+
+var rectIntersects = function(a, b) {
+    var o = a
+    if (b.y > o.y && b.y < o.y + o.image.height) {
+        if (b.x > o.x && b.x < o.x + o.image.width) {
+            return true
+        }
+    }
+    return false
+}
+
+var GuaGame = function(fps) {
+    var g = {
+        actions: {},
+        keydowns: {},
+    }
+    var canvas = document.querySelector('#id-canvas')
+    var context = canvas.getContext('2d')
+    g.canvas = canvas
+    g.context = context
+
+    g.drawImage = function(guaImage) {
+        g.context.drawImage(guaImage.image, guaImage.x, guaImage.y)
+    }
+
+    window.addEventListener('keydown', function(event) {
+        g.keydowns[event.key] = true
+    })
+
+    window.addEventListener('keyup', function(event) {
+        g.keydowns[event.key] = false
+    })
+
+    g.registerAction = function(key, callback) {
+        g.actions[key] = callback
+    }
+
+    setInterval(function() {
+        // event
+        var actions = Object.keys(g.actions)
+        for (var i = 0; i < actions.length; i++) {
+            var key = actions[i]
+            if (g.keydowns[key]) {
+                g.actions[key]()
+            }
+        }
+        g.update()
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        // draw
+        g.draw()
+    }, 1000/fps)
+
+    return g
+}
