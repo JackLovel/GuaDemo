1ac254ae149cd352e3b47028b030690223338905
JackLovel <gogkatsu@outlook.com>
2020-04-05 13:37:08 +0800

    提交

--- 打飞机/scene/main/scene.js
+++ 打飞机/scene/main/scene.js
@@ -1,21 +1,58 @@
+class Player extends GuaImage {
+    constructor(game) {
+        super(game, 'player')
+        this.speed = 10 
+    }
+    update() {
+
+    }
+    moveLeft() {
+        this.x -= this.speed
+    }
+    moveRight() {
+        this.x += this.speed
+    }
+    moveUp() {
+        this.y -= this.speed
+    }
+    moveDown() {
+        this.y += this.speed
+    }
+}
 class Scene extends GuaScene {
     constructor(game) {
         super(game)
         this.setup()
+        this.setupInputs()
     }
     setup() {
         var game = this.game
         this.bg = GuaImage.new(game, 'sky')
-        this.player = GuaImage.new(game, 'player')
         this.cloud = GuaImage.new(game, 'cloud')
+        this.player = Player.new(game)
         this.player.x = 100
         this.player.y = 150
 
         this.addElement(this.bg)
-        this.addElement(this.player)
         this.addElement(this.cloud)
+        this.addElement(this.player)
     }
-
+    setupInputs() {
+        var g = this.game 
+        var s = this 
+        g.registerAction('a', function() {
+            s.player.moveLeft()
+        })
+        g.registerAction('d', function() {
+            s.player.moveRight()
+        })
+        g.registerAction('w', function() {
+            s.player.moveUp()
+        })
+        g.registerAction('s', function() {
+            s.player.moveDown()
+        })
+    }
     update() {
         this.cloud.y += 1
     }
