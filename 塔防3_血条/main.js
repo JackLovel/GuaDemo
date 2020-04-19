a4c48eeecbb48a7ab59a7a66db90fdb5d0f54987
JackLovel <gogkatsu@outlook.com>
2020-04-19 09:34:56 +0800

    update

---
+++ 塔防3_血条/main.js
@@ -0,0 +1,47 @@
+var enableDebugMode = function(game, enable) {
+    if (!enable) {
+        return
+    }
+    window.paused = false
+    window.addEventListener('keydown', function(event){
+        var k = event.key
+        if (k == 'p') {
+            window.paused = !window.paused
+        }
+    })
+    // 控制速度
+    document.querySelector('#id-input-speed').addEventListener('input', function(event) {
+        var input = event.target
+        window.fps = Number(input.value)
+    })
+}
+
+var __main = function() {
+    var images = {
+        // 背景 
+        cave: 'img/cave.png',
+        // flappy bird image 
+        bg: 'bird/bg.png',
+        pipe: 'bird/pipe.png', 
+        ground: 'bird/ground.png',
+        b1: 'bird/b1.png',
+        b2: 'bird/b2.png',
+        b3: 'bird/b3.png',
+        // 
+        t1: 'tiles/t1.png',
+        t2: 'tiles/t2.png',
+        t3: 'tiles/t3.png',
+        t4: 'tiles/t4.png',
+        //
+        gun: 'img/gun.png',
+    }
+
+    var game = GuaGame.instance(window.fps, images, function(g) {
+        // var s = Scene.new(g)
+        var s = SceneTitle.new(g)
+        // var s = SceneEditor.new(g)
+        g.runWithScene(s)
+    })
+}
+
+__main()
---
+++ 塔防3_血条/scene/title/tower.js
@@ -0,0 +1,42 @@
+class Tower1 extends GuaImage {
+    constructor(game, name) {
+        name = name || 'gun'
+        super(game, name)
+        this.setup()
+    }
+    setup() {
+        this.attack = 1 
+        this.range = 50
+        this.target = null 
+    }
+    update() {
+        let target = this.target
+        // TODO，当敌人渐渐运去，你要设置 target = null 
+        if (this.canAttack(target)) {
+            log('攻击敌人')
+            target.被攻击(this.attack)
+            if (target.dead) {
+                this.target = null 
+            }
+        }
+    }
+    canAttack(enemy) {
+        let e = enemy
+        let enemyExist = e !== null && !e.dead
+        if (enemyExist) {
+            let can = this.center().distance(e.center()) < this.range
+            return can 
+        } else {
+            return false 
+        }
+    }
+    findTarget(enemies) {
+        for (let e of enemies) {
+            // do what, 不 do how 
+            if (this.canAttack(e)) {
+                this.target = e 
+                break 
+            }
+        }
+    }
+}
